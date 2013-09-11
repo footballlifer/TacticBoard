af4fa354cc88a5d8feb0b959d56cfa8c4694556d
yeongsu.wu <yeongsu.wu@FS-SO10-NA10018.LGE.NET>
2013-09-11 11:45:13 +0900

    player movement

--- TacticBoard/src/com/example/tb/MainActivity.java
+++ TacticBoard/src/com/example/tb/MainActivity.java
@@ -1,78 +1,75 @@
 package com.example.tb;
 
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnTouchListener {
+	private boolean DEBUG = true;
+	private String TAG = "MainActivity";
+	
+	private final int MAX_PLAYER = 2;
 	private TacticBoard mTacticBoard;
-	
-	private ImageButton mButtonUndo;
-	private ImageButton mButtonO;
-	private ImageButton mButtonX;
-	
 	private ViewGroup mContainer;
-	private ImageView mImageView;
-	
+	private ImageView mImageViewO;
+	private ImageView mImageViewX;
+
 	private int xDelta;
 	private int yDelta;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		mContainer = (ViewGroup) findViewById(R.id.container);
-		mImageView = new ImageView(this);
-		mImageView.setImageResource(R.drawable.ic_launcher);
-		mContainer.addView(mImageView);
 
-		
-		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mImageView
-				.getLayoutParams();
-		layoutParams.leftMargin = 200;
-		layoutParams.topMargin = 200;
-		layoutParams.rightMargin = -50;
-		layoutParams.bottomMargin = -50;
-		mImageView.setLayoutParams(layoutParams);
-		
-		
-		mImageView.setOnTouchListener(this);
-		
+		for (int i = 0; i < MAX_PLAYER; i++) {
+			mImageViewO = new ImageView(this);
+			mImageViewO.setImageResource(R.drawable.o);
+			mContainer.addView(mImageViewO);
+			mImageViewO.setOnTouchListener(this);
+
+			mImageViewX = new ImageView(this);
+			mImageViewX.setImageResource(R.drawable.x);
+			mContainer.addView(mImageViewX);
+			mImageViewX.setOnTouchListener(this);
+
+			RelativeLayout.LayoutParams layoutParamsX = (RelativeLayout.LayoutParams) mImageViewX
+					.getLayoutParams();
+			layoutParamsX.leftMargin = 100;
+			layoutParamsX.topMargin = 0;
+			layoutParamsX.rightMargin = 0;
+			layoutParamsX.bottomMargin = 0;
+			mImageViewX.setLayoutParams(layoutParamsX);
+		}
+
 		mTacticBoard = (TacticBoard) findViewById(R.id.tb);
 		ViewBar vb = new ViewBar(this, mTacticBoard);
-		
+
 		FrameLayout fl = (FrameLayout) findViewById(R.id.frame);
 		fl.addView(vb);
 	}
 
-	
 	public boolean onTouch(View view, MotionEvent event) {
-		final int X = (int) event.getRawX();
-		final int Y = (int) event.getRawY();
+		final int x = (int) event.getRawX();
+		final int y = (int) event.getRawY();
 		switch (event.getAction() & MotionEvent.ACTION_MASK) {
 		case MotionEvent.ACTION_DOWN:
-			RelativeLayout.LayoutParams Params = (RelativeLayout.LayoutParams) view
+			RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view
 					.getLayoutParams();
-			xDelta = X - Params.leftMargin;
-			yDelta = Y - Params.topMargin;
+			xDelta = x - params.leftMargin;
+			yDelta = y - params.topMargin;
 			break;
 		case MotionEvent.ACTION_UP:
-			Toast.makeText(this, "Soltamos", Toast.LENGTH_LONG).show();
-			mImageView = new ImageView(this);
-			mImageView.setImageResource(R.drawable.ic_launcher);
-			mContainer.addView(mImageView);
-			mImageView.setOnTouchListener(this);
-
 			break;
 		case MotionEvent.ACTION_POINTER_DOWN:
 			break;
@@ -81,8 +78,8 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 		case MotionEvent.ACTION_MOVE:
 			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
 					.getLayoutParams();
-			layoutParams.leftMargin = X - xDelta;
-			layoutParams.topMargin = Y - yDelta;
+			layoutParams.leftMargin = x - xDelta;
+			layoutParams.topMargin = y - yDelta;
 			layoutParams.rightMargin = -50;
 			layoutParams.bottomMargin = -50;
 			view.setLayoutParams(layoutParams);
@@ -90,9 +87,9 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 		}
 		mContainer.invalidate();
 		return true;
-	}	
-	
-	
+
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
